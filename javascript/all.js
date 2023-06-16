a42faa7514a7a5e1eee6b4baf7ee5b5c17526a72
Ann Chou <135598409+AnnChouCode@users.noreply.github.com>
2023-06-16 21:09:36 +0800

    Add Image Folder & Swiper

--- javascript/all.js
+++ javascript/all.js
@@ -1,24 +1,54 @@
+// jQuery ---------------------------
+
 $(document).ready(function () {
 
     // PRODUCT SECTION 新到舊按鈕呼叫菜單
-    $('.select_old_new .select_btn').click(function (e) {
-        $('.dropdown_menu').toggleClass('show');
+    $('.select-old-new .select-btn').click(function (e) {
+        $('.dropdown-menu').toggleClass('show');
     });
 
     // PRODUCT SECTION 切換按鈕文字
-    $('.new_to_old').click(function (e) {
+    $('.new-to-old').click(function (e) {
         e.preventDefault();
-        $('.dropdown_menu').toggleClass('show');
-        $('.dropdown_btnText').text('由新到舊');
+        $('.dropdown-menu').toggleClass('show');
+        $('.dropdown-btnText').text('由新到舊');
     });
 
-    $('.old_to_new').click(function (e) {
+    $('.old-to-new').click(function (e) {
         e.preventDefault();
-        $('.dropdown_menu').toggleClass('show');
-        $('.dropdown_btnText').text('由舊到新');
+        $('.dropdown-menu').toggleClass('show');
+        $('.dropdown-btnText').text('由舊到新');
     });
 
     // FOOTER SECTION 回到最上
-    $('.scrollToTop').click(function (e) { $('html,body').animate({scrollTop:0},'slow');
+    $('.scrollToTop').click(function (e) {
+        $('html,body').animate({ scrollTop: 0 }, 'slow');
+    });
 });
-});
+
+
+// ENTERPRISE SECTION swiper --------------------
+const swiper = new Swiper('.swiper', {
+    // Default parameters
+    slidesPerView: 1, //手機版每頁顯示卡片數量
+
+    // RWD Responsive breakpoints
+    breakpoints: {
+      // when window width is >= 768px
+      767.98: {
+        slidesPerView: 2,
+        spaceBetween: 24
+      },
+      // when window width is >= 1200px
+      1199.98: {
+        slidesPerView: 3,
+        spaceBetween: 24
+      }
+    },
+
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets', //點擊換頁前置設定
+        clickable:'true' //可點擊換頁
+      },
+  })
